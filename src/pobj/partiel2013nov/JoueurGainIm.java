1148f2aaf815ddf24e7a7655685344d2d02db0f7
Tloz <scrimet@hotmail.fr>
2013-11-27 15:51:57 +0100

    created a better IA for player. Still improvable thought

---
+++ src/pobj/partiel2013nov/JoueurGainIm.java
@@ -0,0 +1,11 @@
+package pobj.partiel2013nov;
+
+public class JoueurGainIm implements IStrategie{
+
+	@Override
+	public int jouer(Plateau pl, Piece p) throws PasDeCoup {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
